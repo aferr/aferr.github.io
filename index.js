3cc48d7a17df7fb474be2c1f0ae00096fff1096f
Andrew Ferraiuolo <andrewferraiuolo@gmail.com>
2023-08-10 18:00:16 +0100

    first draft of personal website

---
+++ index.js
@@ -0,0 +1,3 @@
+var email = "andrewferr@proton.me";
+var emailElement = document.getElementById("emailplaceholdergoawaybots");
+emailElement.innerHTML = '<a href="mailto:' + email + '">' + email + '</a>';
